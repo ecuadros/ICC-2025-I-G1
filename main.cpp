6ad9a00fd9cc20cc8c7f3fa8674e31635cf4b3d9
Klins18 <102160876+Klins18@users.noreply.github.com>
2025-05-17 12:32:19 -0500

    division

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int division(int a, int b){
+    return a/b;
+}
+
+
 int main() {
-    cout << "Hola, mundo!" << endl;
+    division(8,4);
+    cout << division << endl;
     return 0;
 }
